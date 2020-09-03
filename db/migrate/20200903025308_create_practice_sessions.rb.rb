68f9ed5df9f1c5080006ffb125ac81409ff43429
nikkamille <k.pagibigan@gmail.com>
2020-09-03 01:10:47 -0600

    create practice_sessions table and do migration

---
+++ db/migrate/20200903025308_create_practice_sessions.rb.rb
@@ -0,0 +1,12 @@
+class CreatePracticeSessions.rb < ActiveRecord::Migration
+  def change
+    create_table :practice_sessions do |t|
+      t.date :date
+      t.time :time
+      t.string :practice_item
+      t.string :duration
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
