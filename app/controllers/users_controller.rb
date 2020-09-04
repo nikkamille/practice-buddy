28a4ef57c796147670f4e042403e7838047b4694
nikkamille <k.pagibigan@gmail.com>
2020-09-04 13:59:21 -0600

    fix log in issues and dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,16 @@ class UsersController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            redirect '/practice-sessions'
+            redirect '/dashboard'
         else
             redirect '/login'
         end
+    end
+
+    get '/dashboard' do
+        @user = User.find(session[:user_id])
+        @practice_sessions = PracticeSession.all
+        erb :'users/dashboard'
     end
 
 end
