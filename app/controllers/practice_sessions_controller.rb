c7d03f63afd21513717a1be1338a9b326652365d
nikkamille <k.pagibigan@gmail.com>
2020-09-07 02:29:15 -0600

    creates new practice sessions

--- app/controllers/practice_sessions_controller.rb
+++ app/controllers/practice_sessions_controller.rb
@@ -1,17 +1,7 @@
 class PracticeSessionsController < ApplicationController
 
-    # post '/practice-sessions' do
-
-    #     @user = User.find_by(username: params[:username])
-    #     if @user && @user.authenticate(params[:password])
-    #         session[:user_id] = @user.id
-    #         @practice_sessions = PracticeSession.all
-    #         erb :'practice_sessions/index'
-    #     else
-    #         redirect '/login'
-    #     end
-        
-    # end
-
+    get '/practice-sessions/new' do
+        erb :'practice_sessions/new'
+    end
 
 end
